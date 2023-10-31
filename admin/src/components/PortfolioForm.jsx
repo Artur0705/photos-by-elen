b1384b73cc6bd886beaa512a4559ff9672e29619
Artur0705 <artlil420@gmail.com>
2023-10-31 15:44:34 +1100

    Updated the styles of disabled state

--- admin/src/components/PortfolioForm.jsx
+++ admin/src/components/PortfolioForm.jsx
@@ -23,6 +23,10 @@ const PortfolioForm = ({ portfolioItem, onClose }) => {
   const [errors, setErrors] = useState({});
   const [isUploading, setIsUploading] = useState(false);
 
+  const buttonClasses = isUploading
+    ? "bg-indigo-400 text-white py-2 px-4 rounded cursor-not-allowed"
+    : "bg-indigo-600 text-white py-2 px-4 rounded hover:bg-indigo-700";
+
   useEffect(() => {
     if (imageUrl) {
       setFormData((prevState) => ({
@@ -165,12 +169,36 @@ const PortfolioForm = ({ portfolioItem, onClose }) => {
         >
           Cancel
         </button>
-        <button
-          type="submit"
-          className="bg-indigo-600 text-white py-2 px-4 rounded"
-          disabled={isUploading}
-        >
-          {portfolioItem ? "Update" : "Create"}
+        <button type="submit" className={buttonClasses} disabled={isUploading}>
+          {isUploading ? (
+            <span className="flex items-center">
+              <svg
+                className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <circle
+                  className="opacity-25"
+                  cx="12"
+                  cy="12"
+                  r="10"
+                  stroke="currentColor"
+                  strokeWidth="4"
+                />
+                <path
+                  className="opacity-75"
+                  fill="currentColor"
+                  d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 2.21.896 4.33 2.517 5.938l1.09-1.09c-.909-.909-1.437-2.11-1.506-3.348h1.902z"
+                />
+              </svg>
+              Uploading
+            </span>
+          ) : portfolioItem ? (
+            "Update"
+          ) : (
+            "Create"
+          )}
         </button>
       </div>
     </form>
